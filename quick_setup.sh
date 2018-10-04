762f33d94aae2a3dbae9958895e523f30c5c0960
Junyuan Hong <jyhong836@gmail.com>
2018-10-03 21:17:26 -0400

    Rename the repository and add new stuffs.

---
+++ quick_setup.sh
@@ -0,0 +1,14 @@
+# @Author: Junyuan Hong
+# @Date:   2018-10-03
+# @Last Modified by:   Junyuan Hong
+# @Last Modified time: 2018-10-03
+
+git clone https://github.com/jyhong836/linux_auto_config.git
+cd linux_auto_config
+
+echo setup vim...
+mv vim/* ~/
+
+echo setup zsh...
+mv zsh-syntax-highlighting/ .local/share/zsh-syntax-highlighting
+mv zsh/.zshrc ~/
